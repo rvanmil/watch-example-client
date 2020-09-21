3650acaee0a1ee32174d996e1d9003d7680cf483
Allard van der Ouw <allard.van.der.ouw@oliver-it.nl>
2020-09-21 15:15:14 +0200

    modify react hooks to class components

--- src/App.js
+++ src/App.js
@@ -1,84 +1,79 @@
-import React, { useEffect, useState, useCallback } from 'react'
+import React, { PureComponent } from 'react'
 import { v4 as uuidv4 } from 'uuid'
 import { login, countersCollection } from './realm'
 
 import './App.css'
 
-let interval
-
-function App() {
-  const [user, setUser] = useState(null)
-  const [uuid] = useState(uuidv4())
-  const [isSending, setIsSending] = useState(false)
-  const [values, setValues] = useState([])
-
-  const addValueToValues = useCallback((value) => {
-    values.push(value)
-    setValues([...values].reverse())
-
-    // Stop at 250 (240 = 2 minutes)
-    if (values.length === 250) {
-      clearInterval(interval)
-    }
-  }, [values])
-
-  useEffect(() => {
-    const performLogin = async () => {
-      const user = await login()
-      setUser(user)
-    }
-
-    performLogin()
-  }, [])
-
-  useEffect(() => {
-    const performStart = async () => {
-      if (user && !isSending) {
-        setIsSending(true)
-
-        // Start watch on uuid
-        const stream = await countersCollection.watch({ 'fullDocument.uuid': uuid })
-        stream.onNext((event) => {
-          if (event.operationType === 'insert' && event.fullDocument) {
-            addValueToValues({ date: new Date(), counter: event.fullDocument.value })
-          }
-          if (event.operationType === 'update' && event.fullDocument) {
-            addValueToValues({ date: new Date(), counter: event.fullDocument.value })
-          }
-        })
-
-        // Start counter (update every 0.5 second)
-        interval = setInterval(() => {
-          const query = { uuid }
-          const update = {
-            $set: { uuid },
-            $inc: { value: 0.5 }
-          }
-          const options = { upsert: true }
-          countersCollection.updateOne(query, update, options)
-        }, 500)
-      }
-    }
-
-    performStart()
-  }, [uuid, user, isSending, addValueToValues])
-
-  if (!user) {
-    return null
+class App extends PureComponent {
+  state = {
+    uuid: uuidv4(),
+    user: null,
+    values: []
   }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          UUID: {uuid}
-        </p>
-        {values.map((value, index) => (
-          <p key={index}>{value.date.toISOString()}: {value.counter}</p>
-        ))}
-      </header>
-    </div>
-  )
+  componentDidMount() {
+    const { uuid } = this.state
+    const start = async () => {
+      // Initiate login
+      const user = await login()
+      this.setState({ user })
+
+      // Start watch on uuid
+      const stream = await countersCollection.watch({ 'fullDocument.uuid': uuid })
+      stream.onNext((event) => {
+        if (event.operationType === 'insert' && event.fullDocument) {
+          this.setState({
+            values: [{ date: new Date(), counter: event.fullDocument.value }, ...this.state.values]
+          })
+        }
+        if (event.operationType === 'update' && event.fullDocument) {
+          this.setState({
+            values: [{ date: new Date(), counter: event.fullDocument.value }, ...this.state.values]
+          })
+        }
+
+        // Stop at 250 (240 = 2 minutes)
+        if (this.state.values.length === 250) {
+          clearInterval(this.interval)
+        }
+      })
+
+      // Start counter (update every 0.5 second)
+      this.interval = setInterval(() => {
+        const query = { uuid }
+        const update = {
+          $set: { uuid },
+          $inc: { value: 0.5 }
+        }
+        const options = { upsert: true }
+        countersCollection.updateOne(query, update, options)
+      }, 500)
+    }
+
+    // Start login and counter
+    start()
+  }
+
+  render() {
+    const { uuid, user, values } = this.state
+
+    if (!user) {
+      return null
+    }
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            UUID: {uuid}
+          </p>
+          {values.map((value, index) => (
+            <p key={index}>{value.date.toISOString()}: {value.counter}</p>
+          ))}
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App
